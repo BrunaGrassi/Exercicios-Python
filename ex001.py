557d8caa603316959cedd8c53f988fba8680b2a9
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-11-03 14:40:30 -0300

    Solução exercício 1
    Par ou Ímpar
    
    Escreva um programa que receba um número inteiro e determine se ele é par ou ímpar.

---
+++ ex001.py
@@ -0,0 +1,10 @@
+# criar um algoritmo que receba um numero e determine se ele é par ou impar
+
+input("Digite um número inteiro: ")
+numero = int()
+if (numero % 2) :
+    print("O número informado é par")
+
+else : 
+    print("O número informado é impar")
+
