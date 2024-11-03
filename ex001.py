37cd7780754094eb5d030e16b8e009c010f8756e
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-11-03 14:44:31 -0300

    Correção

--- ex001.py
+++ ex001.py
@@ -1,8 +1,7 @@
 # criar um algoritmo que receba um numero e determine se ele é par ou impar
 
-input("Digite um número inteiro: ")
-numero = int()
-if (numero % 2) :
+numero = int(input("Digite um número inteiro: "))
+if (numero % 2) == 0 :
     print("O número informado é par")
 
 else : 
